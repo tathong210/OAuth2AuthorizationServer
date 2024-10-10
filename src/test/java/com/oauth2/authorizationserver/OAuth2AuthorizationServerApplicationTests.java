564e8143d94701f05a2dbc5b2966328fb249af84
Thong <vthong0210@gmail.com>
2024-10-10 23:28:18 +0700

    init project structure

---
+++ src/test/java/com/oauth2/authorizationserver/OAuth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
